3c282af395ea4d266f05d4732ea1e443b38a8b38
Sophia Sommers <soommers.sophia@gmail.com>
2021-07-18 19:55:10 -0400

    adding more commands

--- hello.rb
+++ hello.rb
@@ -4,3 +4,8 @@ ans = gets
 puts "What's your name?"
 name = gets
 puts "Hello " + name + ", it's nice to meet you."
+if (ans == "Yes") 
+	puts "Since you're new here, let me show you around."
+else 
+	puts "Since you've been here before, you don't need the tour."
+end
