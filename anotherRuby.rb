33f375814792f55b4d7ea70c8e374b5301382362
Sophia Sommers <soommers.sophia@gmail.com>
2021-08-01 19:45:32 -0400

    another ruby file

---
+++ anotherRuby.rb
@@ -0,0 +1,8 @@
+puts "It has been a tiring week"
+puts "Am I right?"
+ans = gets
+if (ans == "Yes") 
+	puts "I'm sorry you're having a long week, too."
+else 
+	puts "Well I'm glad you're feeling so refreshed!"
+end
